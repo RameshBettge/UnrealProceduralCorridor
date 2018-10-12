bfff875f3fdf516d069db6a72358ac23829f050b
RameshBettge <bettge.r@gmx.de>
2018-10-12 14:56:50 +0200

    ADDED: Modular functionality. Unreal crashed and doesn't start. This is an incomplete safety-commit

--- Source/Roguelike/Private/CorridorGenerator.cpp
+++ Source/Roguelike/Private/CorridorGenerator.cpp
@@ -17,19 +17,22 @@ void ACorridorGenerator::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FloorScaler = NewObject<USceneComponent>(this, TEXT("FloorScaler"));
-	RoofScaler = NewObject<USceneComponent>(this, TEXT("RoofScaler"));
-	ZRotator = NewObject<USceneComponent>(this, TEXT("ZRotator"));
-	// Set Empty's location to local zero.
-	FloorScaler->SetRelativeLocation(GetActorLocation());
-	RoofScaler->SetRelativeLocation(GetActorLocation() + FVector(0.f, 0.f, RoofHeight));
-	ZRotator->SetRelativeLocation(GetActorLocation());
+	//FloorScaler = NewObject<USceneComponent>(this, TEXT("FloorScaler"));
+	//RoofScaler = NewObject<USceneComponent>(this, TEXT("RoofScaler"));
+	//ZRotator = NewObject<USceneComponent>(this, TEXT("ZRotator"));
+	//// Set Empty's location to local zero.
+	//FloorScaler->SetRelativeLocation(GetActorLocation());
+	//RoofScaler->SetRelativeLocation(GetActorLocation() + FVector(0.f, 0.f, RoofHeight));
+	//ZRotator->SetRelativeLocation(GetActorLocation());
 
-	UStaticMeshComponent* FloorComponent = InstantiateMesh(Floor, FloorScaler, FName("Floor"));
-	UStaticMeshComponent* RoofComponent = InstantiateMesh(Roof, RoofScaler, FName("Roof"));
-	//UE_LOG(LogTemp, Warning, TEXT("Setting Mesh!"));
+	//UStaticMeshComponent* FloorComponent = InstantiateMesh(Floor, FloorScaler, FName("Floor"));
+	//UStaticMeshComponent* RoofComponent = InstantiateMesh(Roof, RoofScaler, FName("Roof"));
+	////UE_LOG(LogTemp, Warning, TEXT("Setting Mesh!"));
 
-	CreateSupports();
+	//CreateSupports();
+
+
+	CreateModular();
 }
 
 float ACorridorGenerator::GetPlanarMagnitude(FVector V)
@@ -42,47 +45,47 @@ float ACorridorGenerator::GetPlanarMagnitude(FVector V)
 
 void ACorridorGenerator::CreateSupports()
 {
-	int NumberOfSupports = FGenericPlatformMath::FloorToInt(GetPlanarMagnitude(Controller) / RoofSupportSpacing);
+	/*int NumberOfSupports = FGenericPlatformMath::FloorToInt(GetPlanarMagnitude(Controller) / RoofSupportSpacing);
 	InstantiateSupportRow(NumberOfSupports, (Width * 0.5f) - RoofSupportEdgeOffset, "R_");
-	InstantiateSupportRow(NumberOfSupports, (Width * -0.5f) + RoofSupportEdgeOffset, "L_");
+	InstantiateSupportRow(NumberOfSupports, (Width * -0.5f) + RoofSupportEdgeOffset, "L_");*/
 }
 
 void ACorridorGenerator::ClearSupports()
 {
-	for (UMeshComponent* Component : Supports)
-	{
-		Component->UnregisterComponent();
-		Component->DestroyComponent();
-	}
-	Supports.Empty();
+	//for (UMeshComponent* Component : Supports)
+	//{
+	//	Component->UnregisterComponent();
+	//	Component->DestroyComponent();
+	//}
+	//Supports.Empty();
 }
 
 void ACorridorGenerator::InstantiateSupportRow(int NumberOfSupports, float YPos, FString SideName)
 {
-	if (!RoofSupport)
-	{
-		UE_LOG(LogTemp, Error, TEXT("RoofSupport is unassigned!"));
-		return;
-	}
+	//if (!RoofSupport)
+	//{
+	//	UE_LOG(LogTemp, Error, TEXT("RoofSupport is unassigned!"));
+	//	return;
+	//}
 
-	for (int i = 0; i < NumberOfSupports; i++)
-	{
+	//for (int i = 0; i < NumberOfSupports; i++)
+	//{
 
-		FString SupportName = SideName;
-		SupportName += "RoofSupport";
-		SupportName += FString::FromInt(i);
-		UStaticMeshComponent* Support = InstantiateMesh(RoofSupport, ZRotator, FName(*SupportName));
+	//	FString SupportName = SideName;
+	//	SupportName += "RoofSupport";
+	//	SupportName += FString::FromInt(i);
+	//	UStaticMeshComponent* Support = InstantiateMesh(RoofSupport, ZRotator, FName(*SupportName));
 
 
-		float XPos = RoofSupportSpacing * (i + 0.5f);
-		float XPercentage = XPos / GetPlanarMagnitude(Controller);
+	//	float XPos = RoofSupportSpacing * (i + 0.5f);
+	//	float XPercentage = XPos / GetPlanarMagnitude(Controller);
 
-		float ZPos = XPercentage * Controller.Z;
+	//	float ZPos = XPercentage * Controller.Z;
 
-		Support->SetRelativeLocation(FVector(XPos, YPos * 100.f, ZPos));
-		Support->SetRelativeScale3D(FVector(1.f, 1.f, RoofHeight / 100.f));
-		Supports.Add(Support);
-	}
+	//	Support->SetRelativeLocation(FVector(XPos, YPos * 100.f, ZPos));
+	//	Support->SetRelativeScale3D(FVector(1.f, 1.f, RoofHeight / 100.f));
+	//	Supports.Add(Support);
+	//}
 }
 
 UStaticMeshComponent * ACorridorGenerator::InstantiateMesh(UStaticMesh * Mesh, USceneComponent* Parent, FName MeshName)
@@ -110,30 +113,151 @@ UStaticMeshComponent * ACorridorGenerator::InstantiateMesh(UStaticMesh * Mesh, U
 
 
 
+
 // Called every frame
 void ACorridorGenerator::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 	/// TODO: This function has to happen at least once but be only repeated if bGenerateInTick is true --> Extract function
-	FloorScaler->SetRelativeScale3D(FVector(Controller.Size() / 100.f, Width, 1.f));
-	RoofScaler->SetRelativeScale3D(FVector(Controller.Size() / 100.f, Width, 1.f));
+	//FloorScaler->SetRelativeScale3D(FVector(Controller.Size() / 100.f, Width, 1.f));
+	//RoofScaler->SetRelativeScale3D(FVector(Controller.Size() / 100.f, Width, 1.f));
+	//FVector LookDirection = Controller.GetSafeNormal();
+	//FVector PlanarLookDirection = LookDirection;
+	//PlanarLookDirection.Z = 0.f;
+	//PlanarLookDirection = PlanarLookDirection.GetSafeNormal();
+
+	//FloorScaler->SetRelativeRotation(LookDirection.ToOrientationQuat());
+	//RoofScaler->SetRelativeRotation(LookDirection.ToOrientationQuat());
+	//ZRotator->SetRelativeRotation(PlanarLookDirection.ToOrientationQuat());
+
+	//if(bGenerateInTick)
+	//{
+	//	RoofScaler->SetRelativeLocation(GetActorLocation() + FVector(0.f, 0.f, RoofHeight));
+	//	ClearSupports();
+	//	CreateSupports();
+	//}
+}
+
+
+void ACorridorGenerator::CreateModular()
+{
+	if (RoofWidth == 0.f) { RoofWidth = FloorWidth; }
+
+	FloorElement = FCorridorElement("Floor", Floor, 0.f, FloorWidth);
+	RoofElement = FCorridorElement("Roof", Roof, RoofHeight, RoofWidth);
+	CreateElement(FloorElement);
+	CreateElement(RoofElement);
+
+	for (FCorridorElement E : Elements)
+	{
+		CreateElement(E);
+	}
+}
+
+void ACorridorGenerator::CreateElement(FCorridorElement E)
+{
+	USceneComponent* Container = nullptr;
+	if (E.bIsRow)
+	{
+		if (!RowsContainer) { RowsContainer = NewObject<USceneComponent>(this, TEXT("Rows Container")); }
+		Container = RowsContainer;
+		E.Container = Container;
+	}
+	else
+	{
+		FString Name = E.ElementName;
+		auto automi = "String";
+		Name += " Scaler";
+
+
+		Container = NewObject<USceneComponent>(this, *Name);
+		E.Container = Container;
+
+		SetContainerScale(E);
+		SetContainerRotation(E);
+
+		InstantiateMesh(E.Mesh, Container, *E.ElementName);
+	}
+
+	SetContainerPosition(E);
+
+
+}
+
+void ACorridorGenerator::SetContainerPosition(FCorridorElement Element)
+{
+	if (Element.bUseAutoEdgeOffset)
+	{
+		//Element.SideOffset = Element.width *0.5f;
+		Element.SideOffset = AutoEdgeOffset;
+		Element.bEdgeOffset = true;
+	}
+
+	float OffsetMultiplicator = Element.bEdgeOffset ? FloorWidth * 0.5f - Element.SideOffset : Element.SideOffset;
+	FVector Offset = OffsetMultiplicator * FVector::RightVector;
+
+	if (!Element.bIsRow) { Offset += FVector::UpVector * Element.Height; }
+	Element.Container->SetRelativeLocation(GetActorLocation() + Offset);
+
+
+}
+
+void ACorridorGenerator::SetContainerScale(FCorridorElement Element)
+{
+	Element.Container->SetRelativeScale3D(FVector(Controller.Size() * 0.01f, Element.width * 0.01f, 1.f));
+}
+
+void ACorridorGenerator::SetContainerRotation(FCorridorElement Element)
+{
 	FVector LookDirection = Controller.GetSafeNormal();
 	FVector PlanarLookDirection = LookDirection;
 	PlanarLookDirection.Z = 0.f;
 	PlanarLookDirection = PlanarLookDirection.GetSafeNormal();
 
-	FloorScaler->SetRelativeRotation(LookDirection.ToOrientationQuat());
-	RoofScaler->SetRelativeRotation(LookDirection.ToOrientationQuat());
-	ZRotator->SetRelativeRotation(PlanarLookDirection.ToOrientationQuat());
-
-	if(bGenerateInTick)
-	{
-		RoofScaler->SetRelativeLocation(GetActorLocation() + FVector(0.f, 0.f, RoofHeight));
-		ClearSupports();
-		CreateSupports();
-	}
+	Element.Container->SetRelativeRotation(LookDirection.ToOrientationQuat());
 }
 
+void ACorridorGenerator::CreateModularRow(FCorridorElement Element)
+{
+	/*int NumberOfSupports = FGenericPlatformMath::FloorToInt(GetPlanarMagnitude(Controller) / RoofSupportSpacing);
+	InstantiateSupportRow(NumberOfSupports, (Width * 0.5f) - RoofSupportEdgeOffset, "R_");
+	InstantiateSupportRow(NumberOfSupports, (Width * -0.5f) + RoofSupportEdgeOffset, "L_");*/
+}
+
+void ACorridorGenerator::InstantiateModularRow(FCorridorElement E, int NumberOfSupports, float YPos, FString SideName)
+{
+	if (!E.Mesh)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s is unassigned!"), E);
+		return;
+	}
+
+	for (int i = 0; i < NumberOfSupports; i++)
+	{
+
+		FString SupportName = SideName;
+		SupportName += E.ElementName;
+		SupportName += FString::FromInt(i);
+		UStaticMeshComponent* Support = InstantiateMesh(E.Mesh, E.Container, FName(*SupportName));
+
+
+		float XPos = E.Spacing * (i + 0.5f);
+		float XPercentage = XPos / GetPlanarMagnitude(Controller);
+
+		float ZPos = XPercentage * Controller.Z;
+
+		Support->SetRelativeLocation(FVector(XPos, YPos * 100.f, ZPos));
+
+		//if (E.YSize == 0.f) 
+		//{
+		//	//Support->SetRelativeScale3D(FVector(1.f, 1.f, E.YSize / 100.f));
+		//}
+		//else 
+		//{
+			Support->SetRelativeScale3D(FVector(1.f, 1.f, RoofHeight / 100.f));
+		//}
+	}
+}
 
 
--- Source/Roguelike/Public/CorridorGenerator.h
+++ Source/Roguelike/Public/CorridorGenerator.h
@@ -8,56 +8,104 @@
 #include "Components/StaticMeshComponent.h"
 #include "CorridorGenerator.generated.h"
 
+USTRUCT()
+struct FCorridorElement
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	FString ElementName;
+
+	UPROPERTY(EditAnywhere)
+		UStaticMesh* Mesh;
+
+	// Z-Scale for Rows.
+	UPROPERTY(EditAnywhere)
+	float Height;
+
+	// If the side offset should be calculated from the edge instead of from the middle.
+	UPROPERTY(EditAnywhere)
+	bool bUseAutoEdgeOffset = false;
+
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "!bUseAutoEdgeOffset"))
+	bool bEdgeOffset = false;
+
+
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "!bUseAutoEdgeOffset"))
+	float SideOffset = 0.f;
+
+	UPROPERTY(EditAnywhere)
+	bool bIsRow = false;
+
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "!bIsRow"))
+	float width;
+
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "bIsRow"))
+	float Spacing;
+
+	//UPROPERTY(EditAnywhere, meta = (EditCondition = "bIsRow"))
+	//	float YSize = 0.f;
+
+	USceneComponent* Container = nullptr;
+
+	FCorridorElement(FString Name, UStaticMesh* Mesh, float Height, float Width)
+	{
+		this->ElementName = Name, this->Mesh = Mesh, this->Height = Height, this->width = Width;
+	}
+	FCorridorElement() {};
+};
+
+
 UCLASS()
 class ROGUELIKE_API ACorridorGenerator : public AActor
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	virtual void BeginPlay() override;
 	ACorridorGenerator();
 	virtual void Tick(float DeltaTime) override;
 
 public:
-	UPROPERTY(EditAnywhere, Category ="Meshes")
-	UStaticMesh* Floor;
 	UPROPERTY(EditAnywhere, Category = "Meshes")
-	UStaticMesh* Roof;
+		UStaticMesh* Floor;
 	UPROPERTY(EditAnywhere, Category = "Meshes")
-	UStaticMesh* RoofSupport;
+		UStaticMesh* Roof;
+	//UPROPERTY(EditAnywhere, Category = "Meshes")
+	//	UStaticMesh* RoofSupport;
 
-	
-	UPROPERTY(EditAnywhere, Category = "Size Parameters")
-		float Width = 2.f;
+
+	//UPROPERTY(EditAnywhere, Category = "Size Parameters")
+	//	float Width = 2.f;
 
 	UPROPERTY(EditAnywhere, Category = "Size Parameters", meta = (MakeEditWidget))
-	FVector Controller = FVector(50.0f, .0f, 0.f);
+		FVector Controller = FVector(50.0f, .0f, 0.f);
 
-	UPROPERTY(EditAnywhere, Category = "Size Parameters|Roof")
-	float RoofHeight = 200.f;
-	UPROPERTY(EditAnywhere, Category = "Size Parameters|Roof")
-	float RoofSupportSpacing = 100.f;
-	UPROPERTY(EditAnywhere, Category = "Size Parameters|Roof")
-		float RoofSupportEdgeOffset = 30.f;
+	//UPROPERTY(EditAnywhere, Category = "Size Parameters|Roof")
+	//	float RoofHeight = 200.f;
+	//UPROPERTY(EditAnywhere, Category = "Size Parameters|Roof")
+	//	float RoofSupportSpacing = 100.f;
+	//UPROPERTY(EditAnywhere, Category = "Size Parameters|Roof")
+	//	float RoofSupportEdgeOffset = 30.f;
 
-	UPROPERTY(EditAnywhere, Category = "Debugging")
-		TArray<UStaticMeshComponent*> Supports;
-	UPROPERTY(EditAnywhere, Category = "Debugging")
-		bool bGenerateInTick;
+	//UPROPERTY(EditAnywhere, Category = "Debugging")
+	//	TArray<UStaticMeshComponent*> Supports;
+	//UPROPERTY(EditAnywhere, Category = "Debugging")
+	//	bool bGenerateInTick;
 
-	
+
 
 private:
 	UPROPERTY(meta = (MakeEditWidget))
-	FVector Root = FVector::ZeroVector;
+		FVector Root = FVector::ZeroVector;
 
 	/// UProperties are set so I can move the Containers.
 	UPROPERTY(EditAnywhere, Category = "Debugging")
-	USceneComponent* ZRotator;
+		USceneComponent* ZRotator;
 	UPROPERTY(EditAnywhere, Category = "Debugging")
-	USceneComponent* FloorScaler;
+		USceneComponent* FloorScaler;
 	UPROPERTY(EditAnywhere, Category = "Debugging")
-	USceneComponent* RoofScaler;
+		USceneComponent* RoofScaler;
 
 private:
 	float GetPlanarMagnitude(FVector);
@@ -66,4 +114,33 @@ private:
 	void ClearSupports();
 	void InstantiateSupportRow(int NumberOfSupports, float YPos, FString SideName);
 	UStaticMeshComponent* InstantiateMesh(UStaticMesh* Mesh, USceneComponent* Parent, FName MeshName = TEXT("Mesh"));
+
+
+
+	UPROPERTY(EditAnywhere, Category = "Modular")
+	float FloorWidth;
+	UPROPERTY(EditAnywhere, Category = "Modular")
+	float RoofHeight;
+	UPROPERTY(EditAnywhere, Category = "Modular")
+	float RoofWidth;
+
+	UPROPERTY(EditAnywhere, Category = "Modular")
+		float AutoEdgeOffset= 50.f;
+
+	UPROPERTY(EditAnywhere, Category = "Modular")
+	TArray<FCorridorElement> Elements;
+	FCorridorElement FloorElement;
+	FCorridorElement RoofElement;
+
+	TArray<USceneComponent*> Containers;
+	USceneComponent* RowsContainer = nullptr;
+
+	void CreateModular();
+	void CreateElement(FCorridorElement E);
+	void SetContainerPosition(FCorridorElement Element);
+	void SetContainerScale(FCorridorElement Element);
+	void SetContainerRotation(FCorridorElement Element);
+
+	void CreateModularRow(FCorridorElement Element);
+	void InstantiateModularRow(FCorridorElement E, int NumberOfSupports, float YPos, FString SideName);
 };
